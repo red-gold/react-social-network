833106ad8d568e19ba4e5cf6816b2f274840117d
Qolzam <amir.gholzam@live.com>
2017-10-21 10:53:20 +0700

    Add index for actions folder to make shorter path for actions

---
+++ app/actions/index.ts
@@ -0,0 +1,21 @@
+import * as authorizeActions from './authorizeActions'
+import * as circleActions from './circleActions'
+import * as commentActions from './commentActions'
+import * as globalActions from './globalActions'
+import * as imageGalleryActions from './imageGalleryActions'
+import * as notifyActions from './notifyActions'
+import * as postActions from './postActions'
+import * as userActions from './userActions'
+import * as voteActions from './voteActions'
+
+export {
+  authorizeActions,
+  circleActions,
+  commentActions,
+  globalActions,
+  imageGalleryActions,
+  notifyActions,
+  postActions,
+  userActions,
+  voteActions
+}
